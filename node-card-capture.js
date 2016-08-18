56e2c595b7de44350f262280abc0b1f40b10ea45
channeljuannews <jruel006@ucr.edu>
2016-08-18 12:41:15 -0700

    testing this

--- node-card-capture.js
+++ node-card-capture.js
@@ -1,17 +1,11 @@
 'use strict'
 
-let exec = require('child_process').exec
+let exec = require('child_process').execelect
 
 var cardCapture = function(callback) {
     console.log('The process is happeing in ' + __dirname)
     var trackdata = exec('sudo python ' + __dirname + '/card-reader.py').toString('utf8')
     callback(trackdata)
 }
-
-
-cardCapture(function(data){
-    console.log(data)
-})
-
 
 module.exports = cardCapture
