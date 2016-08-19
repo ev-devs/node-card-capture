2a4ea39dfcc2aebc3a092da9efe22b8642541242
channeljuannews <jruel006@ucr.edu>
2016-08-19 11:45:27 -0700

    added support for async

--- node-card-capture.js
+++ node-card-capture.js
@@ -1,11 +1,32 @@
 'use strict'
 
 let exec = require('child_process').exec
+let execSync =  require('child_process').execSync
 
 var cardCapture = function(callback) {
-    console.log('The process is happeing in ' + __dirname)
-    var trackdata = exec('sudo python ' + __dirname + '/card-reader.py').toString('utf8')
-    callback(trackdata)
+
+    exec('sudo python ' + __dirname + "/card-reader.py", function(err, stdout, stderr){
+        if (err){
+            console.log('there was an arrororooro')
+        }
+        else {
+            if (stderr) {
+                callback ( "ERROR " + stderr)
+            }
+            else if (stdout) {
+                callback (stdout)
+            }
+        }
+    })
 }
+
+var cardCaptureSync = function() {
+    return  execSync('sudo python ' + __dirname + '/card-reader.py').toString('utf8')
+}
+
+
+cardCapture(function(stuffs){
+    console.log(stuffs)
+})
 
 module.exports = cardCapture
